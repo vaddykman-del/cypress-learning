2d443c98117096ab10dfbb825fd88b9f87671875
Vadym Dykman <helterskelter672@gmail.com>
2026-04-24 14:15:36 +0300

    cleaning code

--- cypress/pages/Login.js
+++ cypress/pages/Login.js
@@ -3,23 +3,20 @@ export class Login {
     cy.visit("https://www.edu.goit.global/account/login");
   }
   validateLoginTitle() {
-    cy.contains("h2", "Login").should("be.visible");
-    cy.contains("h2", "Login").should("have.text", "Login");
+    cy.contains("h2", "Login").should("be.visible").and("have.text", "Login");
   }
   validateInputs() {
     cy.get("#user_email").should("be.visible");
     cy.get("#user_password").should("be.visible");
   }
   validateButton() {
-    cy.get(".eckniwg2").should("be.visible");
+    cy.get("[type='submit']").should("be.visible");
   }
 
   validatePasswordLink() {
-    cy.get('a[href="/account/password/restore"]').should("be.visible");
-    cy.get('a[href="/account/password/restore"]').should(
-      "have.text",
-      "I can't remember the password",
-    );
+    cy.get('a[href="/account/password/restore"]')
+      .should("be.visible")
+      .and("have.text", "I can't remember the password");
   }
 
   validateIncorrectCredsMessage() {
